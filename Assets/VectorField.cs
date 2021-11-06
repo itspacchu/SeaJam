458cfe5b56dd5f1002661fc14d16b882dc23e73a
itspacchu <37984032+itspacchu@users.noreply.github.com>
2021-11-06 10:30:22 +0000

    backup

--- Assets/VectorField.cs
+++ Assets/VectorField.cs
@@ -2,31 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class VectorField {
-    struct Vec3 {
-        public float x, y, z;
-        public Vec3(float x, float y, float z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-        public static Vec3 operator +(Vec3 a, Vec3 b) {
-            return new Vec3(a.x + b.x, a.y + b.y, a.z + b.z);
-        }
-        public static Vec3 operator -(Vec3 a, Vec3 b) {
-            return new Vec3(a.x - b.x, a.y - b.y, a.z - b.z);
-        }
-        public static Vec3 operator *(Vec3 a, float b) {
-            return new Vec3(a.x * b, a.y * b, a.z * b);
-        }
-        public static Vec3 operator /(Vec3 a, float b) {
-            return new Vec3(a.x / b, a.y / b, a.z / b);
-        }
+    struct Svec3 {
+        float xcomp;
+        float ycomp;
+        float zcomp;
     };
 
     List<float> FieldCoefficients;
-    List<Vec3> FieldDirections;
-    public VectorField() {
-        this.FieldDirections = new List<Vec3>();
+
+    public float dt = 0;
+
+    public SVec3 GetField(SVec3 inputVec,float TimeElapsed) {
+        
     }
     
 };
